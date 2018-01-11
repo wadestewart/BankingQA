988cc27a2b34081f1e553db4cca1b04b9cc8fd5e
Wade Stewart <stew.wade@gmail.com>
2018-01-11 12:19:19 -0500

    README image test

--- controllers/users.js
+++ controllers/users.js
@@ -6,14 +6,14 @@ function getSignup (req, res, next) {
 }
 
 // POST signup - function to authenticate users and redirect based on data
-function postSignup (req, res, next) {
+function postSignup (req, res) {
     let signupStrategy = passport.authenticate('local-signup', { // naming convention from passport.js
         successRedirect : '/',
         failureRedirect : '/signup',
         failureFlash : true
     })
 
-    return signupStrategy(req, res, next)
+    return signupStrategy(req, res)
 }
 
 // GET login - function to render login form
@@ -22,14 +22,14 @@ function getLogin (req, res, next) {
 }
 
 // POST login - function to handle login form when submitted
-function postLogin (req, res, next) {
+function postLogin (req, res) {
     let loginProperty = passport.authenticate('local-login', { // naming convention from passport.js
         successRedirect : '/topics',
         failureRedirect : '/login',
         failureFlash : true
     })
 
-    return loginProperty(req, res, next)
+    return loginProperty(req, res)
 }
 
 // GET logout - function for a user to logout and redirect to home page
