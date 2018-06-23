e25f92444f20a562f5dc3ba870b381f857ffe55c
Wade Stewart <stew.wade@gmail.com>
2018-06-23 12:17:39 -0400

    mobile sidenav functional

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav()
+  })
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav')
+//     var instances = M.Sidenav.init(elems, options)
+//   })
