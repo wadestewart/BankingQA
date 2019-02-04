8f54ad03f33106e27a549c0d1dce12b854004c68
Wade Stewart <stew.wade@gmail.com>
2019-02-04 09:16:03 -0500

    updated to connection file to console log db name

--- db/connection.js
+++ db/connection.js
@@ -9,21 +9,18 @@ mongoose.Promise = Promise
 // Set the URI for connecting to local MongoDb
 const mongoUri = 'mongodb://localhost/bankingqa'
 if (process.env.NODE_ENV === 'production') {
-    mongoose.connect(process.env.MLAB_URL, { useMongoClient: true })
-    .then(connection => console.log(`Connection established to '${connection.db.databaseName}'`))
+    mongoose
+    .connect(process.env.MLAB_URL)
+    .then(mongoose => console.log(`Connection established to '${mongoose.connection.db.databaseName}'`))
     .catch(connectionError => console.log('Connection Failed!', connectionError))
   } else {
-    mongoose.createConnection(mongoUri)
-    .then(connection => console.log(`Connection established to '${connection.db.databaseName}'`))
-    .catch(connectionError => console.log('Connection Failed!', connectionError))
+    // Connect to database, with imported mongoose instance
+    mongoose
+        .connect(mongoUri)
+        .then(connection => console.log(`Connection established to '${mongoose.connection.db.databaseName}'`))
+        .catch(connectionError => console.log('Connection Failed!', connectionError))
   }
-
-// Connect to database, with imported mongoose instance
-// mongoose
-//     .connect(mongoUri, {useMongoClient: true})
-//     .then(connection => console.log(`Connection established to '${connection.db.databaseName}'`))
-//     .catch(connectionError => console.log('Connection Failed!', connectionError))
-// Mongoose instance now has a configured connection to our local db, in addition to it's model configuration
+    // Mongoose instance now has a configured connection to our local db, in addition to it's model configuration
 
 // Exporting for use in other files
 module.exports = mongoose
