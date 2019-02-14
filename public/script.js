206281b33ed365cc937d04681f4ca9bf7fa3d73a
Wade Stewart <stew.wade@gmail.com>
2019-02-14 17:31:32 -0500

    removed margin from all page images, working app - moving this app to another branch

--- public/script.js
+++ public/script.js
@@ -2,7 +2,3 @@ $(document).ready(function(){
     $('.sidenav').sidenav()
   })
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav')
-//     var instances = M.Sidenav.init(elems, options)
-//   })
